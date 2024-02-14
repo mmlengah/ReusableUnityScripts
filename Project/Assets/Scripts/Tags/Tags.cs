2876a56054df95b1af742a61406efc5718288b45
Matthius Mlengah <98457199+mmlengah@users.noreply.github.com>
2024-02-14 16:15:57 +0000

    add tags script

---
+++ Project/Assets/Scripts/Tags/Tags.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityTags
+{
+    public enum Tags
+    {
+        Untagged,
+        Respawn,
+        Finish,
+        EditorOnly,
+        MainCamera,
+        Player,
+        GameController
+    }
+}
+
