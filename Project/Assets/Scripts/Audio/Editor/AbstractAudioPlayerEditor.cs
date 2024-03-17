913b4a148aab55553df83dc4d86d70a2c4606a65
Matthius Mlengah <98457199+mmlengah@users.noreply.github.com>
2024-03-17 18:02:11 +0000

    add logger

--- Project/Assets/Scripts/Audio/Editor/AbstractAudioPlayerEditor.cs
+++ Project/Assets/Scripts/Audio/Editor/AbstractAudioPlayerEditor.cs
@@ -1,9 +1,6 @@
 #if UNITY_EDITOR
-using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
-using System;
-using System.Linq;
 
 namespace GameAudio
 {
---
+++ Project/Assets/Scripts/Logger/Logger.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Logger
+{
+    [SerializeField] private bool enableLogging = true;
+
+    private void LogInternal(object message, UnityEngine.Object context, System.Action<object, UnityEngine.Object> logMethod)
+    {
+        if (!enableLogging) return;
+
+#if UNITY_EDITOR
+        string contextName = context != null ? $"{context.name}: " : "";
+        logMethod($"{contextName}{message}", context);
+#endif
+    }
+
+    public void Log(object message, UnityEngine.Object context = null)
+    {
+        LogInternal(message, context, Debug.Log);
+    }
+
+    public void LogWarning(object message, UnityEngine.Object context = null)
+    {
+        LogInternal(message, context, Debug.LogWarning);
+    }
+
+    public void LogError(object message, UnityEngine.Object context = null)
+    {
+        LogInternal(message, context, Debug.LogError);
+    }
+}
